0191449b08937e06696bf53450692559dda04542
Vladimir Orlov <graf.orlov2005@gmail.com>
2026-04-20 18:15:13 +0300

    final fix

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+  await app.listen(port);
 }
 bootstrap();
